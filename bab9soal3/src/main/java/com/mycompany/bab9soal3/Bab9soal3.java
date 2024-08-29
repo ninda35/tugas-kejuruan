4a242884f726eb5f1aa29734a0cd5fb09f7e1c85
ninda35 <144987809+ninda35@users.noreply.github.com>
2024-08-29 08:59:40 +0700

    Add files via upload

---
+++ bab9soal3/src/main/java/com/mycompany/bab9soal3/Bab9soal3.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab9soal3;
+
+/**
+ *
+ * @author Andriyansyah
+ */
+public class Bab9soal3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 1;
+        while (i < 6) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
