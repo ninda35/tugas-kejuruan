83e97704c53cdbfff3434cd515cd58abbd25a725
ninda35 <144987809+ninda35@users.noreply.github.com>
2024-08-29 13:19:07 +0700

    Add files via upload

---
+++ Bab11Soall1/src/main/java/com/mycompany/bab11soall1/Bab11Soall1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab11soall1;
+
+/**
+ *
+ * @author Andriyansyah
+ */
+public class Bab11Soall1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+          Karyawan karyawan1 = new Karyawan("1111", "Ninda", "A", "Manager", 5000000);
+        
+        // Menampilkan informasi karyawan
+        karyawan1.tampilkanInfo();
+    }
+}
