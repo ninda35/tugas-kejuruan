5c1831b85d74fe115c7250d75db51ff588da7f18
ninda35 <144987809+ninda35@users.noreply.github.com>
2024-08-29 08:50:19 +0700

    Add files via upload

--- bab3soal2/src/main/java/com/mycompany/bab3soal2/Bab3soal2.java
+++ bab3soal2/src/main/java/com/mycompany/bab3soal2/Bab3soal2.java
@@ -10,17 +10,20 @@ package com.mycompany.bab3soal2;
  */
 public class Bab3soal2 {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        //TODO code application logic here
+        // TODO code application logic here
         int NIM = 2110117000;
         String mahasiswa = "Andika Jaya Utama";
-        String kelas = "0lSLFED20";
+        String kelas = "01SIFE020";
         float usia = 19.5f;
         double tinggi = 1.72;
         
-        System.out.println("NIM : " + NIM);
+        System.out.println("Nim : " + NIM);
         System.out.println("Nama Mahasiswa : " + mahasiswa);
-        System.out.println("Kekas : " + kelas);
+        System.out.println("Kelas : " + kelas);
         System.out.println("Usia : " + usia);
         System.out.println("Tinggi : " + tinggi);
     }
