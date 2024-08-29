651b7627a36b8ce5ea318bf0c31491abfaf5175e
ninda35 <144987809+ninda35@users.noreply.github.com>
2024-08-29 08:37:37 +0700

    Add files via upload

---
+++ bab3soal1/src/main/java/com/mycompany/bab3soal1/Bab3soal1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab3soal1;
+
+/**
+ *
+ * @author Andriyansyah
+ */
+public class Bab3soal1 {
+    /**
+     * @param args the command line arguments
+     */
+public static void main(String[] args) {
+        //TODO code application logic here
+        int Varl = 9;
+        float Var2 = 8.99f;
+        char Var3 = 'A';
+        boolean Var4 = false;
+        String Var5 = "Hello World";
+        
+        System.out.println(Varl);
+        System.out.println(Var2);
+        System.out.println(Var3);
+        System.out.println(Var4);
+        System.out.println(Var5);
+        
+    }
+}
