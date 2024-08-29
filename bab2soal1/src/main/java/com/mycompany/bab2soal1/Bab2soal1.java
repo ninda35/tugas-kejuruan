6faedb9f595ba1064460bf4fc82323e71cacbf93
ninda35 <144987809+ninda35@users.noreply.github.com>
2024-08-29 08:36:08 +0700

    Add files via upload

---
+++ bab2soal1/src/main/java/com/mycompany/bab2soal1/Bab2soal1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab2soal1;
+
+/**
+ *
+ * @author Andriyansyah
+ */
+public class Bab2soal1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int a= 25;
+        int b= 25;
+        int c= a + b;
+        System.out.println(c);
+    }
+}
