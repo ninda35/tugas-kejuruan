07ad1c92bf7f0973e1821188d7c514e4dd7facf7
ninda35 <144987809+ninda35@users.noreply.github.com>
2024-08-29 13:20:05 +0700

    Add files via upload

---
+++ karyawan/src/main/java/com/mycompany/karyawan/Karyawan.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.karyawan;
+
+/**
+ *
+ * @author Andriyansyah
+ */
+public class Karyawan {
+           private String idKaryawan;
+           private String namaKaryawan;
+           private String golongan;
+           private String jabatan;
+           private double gajiPokok;
+           
+           //constructor dengan 2 parameter
+           public Karyawan(String idKaryawan, String namaKaryawan, String golongan, String jabatan, double gajiPokok ){
+           this.idKaryawan = idKaryawan;
+           this.namaKaryawan = namaKaryawan;
+           this.golongan = golongan;
+           this.jabatan = jabatan;
+           this.gajiPokok = gajiPokok;
+    }
+        public void tampilkanInfo(){
+        System.out.println("ID Karyawan   : " + idKaryawan);
+        System.out.println("Nama Karyawan : " + namaKaryawan);
+        System.out.println("Golongan      : " + golongan);
+        System.out.println("Jabatan       : " + jabatan);
+        System.out.println("Gaji Pokok    : " + gajiPokok);
+    }
+}
