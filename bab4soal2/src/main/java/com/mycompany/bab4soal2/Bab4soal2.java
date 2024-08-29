72a4aa985331652239751f7d2ce0b98028402a4e
ninda35 <144987809+ninda35@users.noreply.github.com>
2024-08-29 08:52:12 +0700

    Add files via upload

---
+++ bab4soal2/src/main/java/com/mycompany/bab4soal2/Bab4soal2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab4soal2;
+
+/**
+ *
+ * @author Andriyansyah
+ */
+public class Bab4soal2 {
+
+    public static void main(String[] args) {
+        //TODO code application logic here 
+        int w, x, y, z;
+        w = 5;
+        x = 6;
+        y = 8-x++;
+        z = 8-++w;
+        
+        System.out.println(w);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+}
