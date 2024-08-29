d57e06db34a19b5ee4dc1f0e6b8c0a34ac4e6299
ninda35 <144987809+ninda35@users.noreply.github.com>
2024-08-29 08:55:27 +0700

    Add files via upload

---
+++ bab7soal1/src/main/java/com/mycompany/bab7soal1/Bab7soal1.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab7soal1;
+
+/**
+ *
+ * @author Andriyansyah
+ */
+public class Bab7soal1 {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //TODO code application logic here 
+        int day = 2;
+        switch (day) {
+            case 1:
+                System.out.println("Sabtu");
+                break;
+            case 2:
+                System.out.println("Minggu");
+                break;
+            default:
+                System.out.println("Hari tidak valid");
+                break;
+        }
+    }
+} 
+    
+    
+
