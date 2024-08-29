99ec084efa0df33f924ac3ce2744535ac45a6d90
ninda35 <144987809+ninda35@users.noreply.github.com>
2024-08-29 08:51:23 +0700

    Add files via upload

---
+++ bab4soal1111/src/main/java/com/mycompany/bab4soal1111/Bab4soal1111.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab4soal1111;
+
+/**
+ *
+ * @author Andriyansyah
+ */
+public class Bab4soal1111 {
+
+    public static void main(String[] args) {
+        //TODO code application logic here
+        int angkal = 10;
+        int angka2 = 3;
+        
+        System.out.println("Hasil Pertambahan: " + (angkal + angka2 ));
+        System.out.println("Hasil Pengurangan: " + (angkal - angka2 ));
+        System.out.println("Hasil Perkalian: " + (angkal * angka2 ));
+        System.out.println("Hasil Pembagian: " + (angkal / angka2 ));
+    }
+}
