a82967441a1c43185802578677b37333e409353b
ninda35 <144987809+ninda35@users.noreply.github.com>
2024-08-29 08:36:54 +0700

    Add files via upload

---
+++ bab2soal2/src/main/java/com/mycompany/bab2soal2/Bab2soal2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab2soal2;
+
+/**
+ *
+ * @author Andriyansyah
+ */
+public class Bab2soal2 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String makanan = "ayam geprek";
+        String minuman = "jus jambu";
+                
+        System.out.println("makanan favorit saya : " + makanan);
+        System.out.println("minuman favorit saya : " + minuman);
+    }
+}
