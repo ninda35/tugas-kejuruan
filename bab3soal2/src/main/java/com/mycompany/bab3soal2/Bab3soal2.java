98d3dc8a2cd8e111f358f194a20970663e8036ab
ninda35 <144987809+ninda35@users.noreply.github.com>
2024-08-29 08:38:28 +0700

    Add files via upload

---
+++ bab3soal2/src/main/java/com/mycompany/bab3soal2/Bab3soal2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab3soal2;
+
+/**
+ *
+ * @author Andriyansyah
+ */
+public class Bab3soal2 {
+
+    public static void main(String[] args) {
+        //TODO code application logic here
+        int NIM = 2110117000;
+        String mahasiswa = "Andika Jaya Utama";
+        String kelas = "0lSLFED20";
+        float usia = 19.5f;
+        double tinggi = 1.72;
+        
+        System.out.println("NIM : " + NIM);
+        System.out.println("Nama Mahasiswa : " + mahasiswa);
+        System.out.println("Kekas : " + kelas);
+        System.out.println("Usia : " + usia);
+        System.out.println("Tinggi : " + tinggi);
+    }
+}
