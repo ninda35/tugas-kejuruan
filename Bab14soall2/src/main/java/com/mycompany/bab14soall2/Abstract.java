886a0a8332345da8c5c287510fe653a08eff9007
ninda35 <144987809+ninda35@users.noreply.github.com>
2024-08-30 10:45:40 +0700

    Add files via upload

---
+++ Bab14soall2/src/main/java/com/mycompany/bab14soall2/Abstract.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bab14soall2;
+
+/**
+ *
+ * @author Andriyansyah
+ */
+public class Abstract {
+    
+}
---
+++ Bab14soall2/src/main/java/com/mycompany/bab14soall2/Bab14soall2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab14soall2;
+
+/**
+ *
+ * @author Andriyansyah
+ */
+public class Bab14soall2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Burung burung = new Burung();
+       
+       burung.suara();
+       
+       burung.terbang();
+    }
+}
